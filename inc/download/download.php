49e8bf55f84539cc6369edac0f686dbbb2661e51
Almhdy <47367241+Almhdy@users.noreply.github.com>
2023-11-07 11:31:47 +0000

    Add files via upload

---
+++ inc/download/download.php
@@ -0,0 +1,22 @@
+<?php
+// Check HTTP Request
+if (isset($_GET['url']) && strpos($_GET['url'], 'i.ytimg.com') == true) {
+    ob_start();
+    // Build Query URL
+    $url = $_GET['url'];
+    $content_type = 'image.jpg';
+    // Force download
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.$content_type.'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Pragma: public');
+    ob_clean();
+    flush();
+    readfile($url);
+    exit;
+    // Else Redirect
+} else {
+    header('location: /');
+}
